4ea8e360a2b0cbc4e1bcc6cd7fcec2d88c678707
kim-macbook <creator@hanyang.ac.kr>
2020-10-04 07:29:41 +0900

    Add Popover component

--- src/components/home/Friend.tsx
+++ src/components/home/Friend.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Menu } from 'antd';
 import styled from 'styled-components';
+import { Button, Menu, Popover, Space } from 'antd';
+import { UserDeleteOutlined, MessageOutlined } from '@ant-design/icons';
 import { UserResponse } from '../../@types';
 import StyledAvatar from '../common/StyledAvatar';
 
@@ -11,11 +12,23 @@ type FriendProps = {
 const { Item } = Menu;
 
 function Friend({ user, ...props }: FriendProps) {
+  const content = (
+    <Space style={{ width: '100%' }} direction="vertical">
+      <StyledButton type="primary" icon={<MessageOutlined />}>
+        채팅하기
+      </StyledButton>
+      <StyledButton type="primary" icon={<UserDeleteOutlined />} danger>
+        삭제하기
+      </StyledButton>
+    </Space>
+  );
   return (
-    <StyledItem {...props}>
-      <StyledAvatar src={user.avatarUrl} size="small" />
-      <div className="username">{user.username}</div>
-    </StyledItem>
+    <Popover placement="right" title={user.username} content={content} trigger="click">
+      <StyledItem {...props}>
+        <StyledAvatar src={user.avatarUrl} size="small" />
+        <div className="username">{user.username}</div>
+      </StyledItem>
+    </Popover>
   );
 }
 
@@ -27,6 +40,10 @@ const StyledItem = styled(Item)`
     flex: 1;
     padding-left: 1rem;
   }
+`;
+
+const StyledButton = styled(Button)`
+  width: 100%;
 `;
 
 export default Friend;
